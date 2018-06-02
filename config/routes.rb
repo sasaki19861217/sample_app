153f06dfa129bb630dbe1aefbbfd2f28de0a40eb
EC2 Default User <ec2-user@ip-172-31-8-240.us-east-2.compute.internal>
2018-06-02 07:18:17 +0000

    Finish user follow / unfollow actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ SampleApp::Application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
+  resources :users, only: [:show, :index, :destroy] do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users, only: [:show, :index, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
   # match '/',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
